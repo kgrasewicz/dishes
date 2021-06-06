15e62246bc0ac9d53556ce1d22ac10a4751b9c27
Katarzyna Grasewicz <grasewiczkatarzyna@gmail.com>
2021-06-06 14:56:51 +0200

    remove unused files

---
+++ src/components/background/Background.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="app__background">
+        <h1>Add ingredient</h1>
+        
+    </div>
+  );
+}
+
+export default App;
