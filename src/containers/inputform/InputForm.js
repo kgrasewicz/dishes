b98c4623b6f0306ea54cf8ba6609023f4eb0d8a0
Katarzyna Grasewicz <grasewiczkatarzyna@gmail.com>
2021-06-06 17:48:37 +0200

    style input component, add svg images, add custom checkboxes

--- src/containers/inputform/InputForm.js
+++ src/containers/inputform/InputForm.js
@@ -1,66 +1,60 @@
-import React from 'react'
-import { Field, reduxForm } from 'redux-form'
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+import pizza from "./pizza.svg";
+import soup from "./soup.svg";
+import sandwich from "./sandwich.svg";
 
 const inputForm = (props) => {
-  const { handleSubmit, pristine, reset, submitting } = props
+  const { handleSubmit } = props;
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>What dish would you like to add?</label>
-        <div>
-          <Field name="dishName" component="input" type="text" placeholder="Dish name"/>
+    <form className="app__form" onSubmit={handleSubmit}>
+      <div className="app__form__scroll">
+      <label className="app__form__dish">
+        <h3>Dish name</h3>
+        <Field
+          name="dishName"
+          component="input"
+          type="text"
+          placeholder="Enter dish name"
+        />
+      </label>
+      <label className="app__form__time">
+        <h3>Preparation time</h3>
+        <Field
+          name="prepTime"
+          component="input"
+          type="time"
+          placeholder="00:00:00"
+          step="1"
+        />
+      </label>
+      <div className="app__form__type">
+        <h3>Type of dish</h3>
+        <div className="app__form__type__options">
+          <label htmlFor="pizza">
+            <img src={pizza} alt="pizza" />
+            <h2>Pizza</h2>
+            <Field name="type" id="pizza" component="input" type="radio" />
+          </label>
+          <label htmlFor="soup">
+            <img src={soup} alt="pizza" />
+
+            <h2>Soup</h2>
+            <Field name="type" id="soup" component="input" type="radio" />
+          </label>
+          <label htmlFor="sandwich">
+            <img src={sandwich} alt="pizza" />
+            <h2>Sandwich</h2>
+            <Field name="type" id="sandwich" component="input" type="radio" />
+          </label>
         </div>
       </div>
-      <div>
-        <label>Preparation time</label>
-        <div>
-          <Field name="prepTime" component="input" type="text" placeholder="Last Name"/>
-        </div>
-      </div>
-      <div>
-        <label>Email</label>
-        <div>
-          <Field name="email" component="input" type="email" placeholder="Email"/>
-        </div>
-      </div>
-      <div>
-        <label>Sex</label>
-        <div>
-          <label><Field name="sex" component="input" type="radio" value="male"/> Male</label>
-          <label><Field name="sex" component="input" type="radio" value="female"/> Female</label>
-        </div>
-      </div>
-      <div>
-        <label>Favorite Color</label>
-        <div>
-          <Field name="favoriteColor" component="select">
-            <option></option>
-            <option value="ff0000">Red</option>
-            <option value="00ff00">Green</option>
-            <option value="0000ff">Blue</option>
-          </Field>
-        </div>
-      </div>
-      <div>
-        <label htmlFor="employed">Employed</label>
-        <div>
-          <Field name="employed" id="employed" component="input" type="checkbox"/>
-        </div>
-      </div>
-      <div>
-        <label>Notes</label>
-        <div>
-          <Field name="notes" component="textarea"/>
-        </div>
-      </div>
-      <div>
-        <button type="submit" disabled={pristine || submitting}>Submit</button>
-        <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
       </div>
     </form>
-  )
-}
+  );
+};
 
 export default reduxForm({
-  form: 'simple'  // a unique identifier for this form
-})(inputForm)
+  form: "simple", // a unique identifier for this form
+})(inputForm);
